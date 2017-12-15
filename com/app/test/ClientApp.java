cd80e9770205cd7cc9ca47eaa4f917a1e1198f50
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-15 23:57:10 +0530

    Create ClientApp.java

---
+++ com/app/test/ClientApp.java
@@ -0,0 +1,17 @@
+package com.app.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.app.beans.Employee;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		//read and print the employee i.e parent object
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("com/app/cfg/Springconfig.xml");
+		Employee e=(Employee)ctx.getBean("emp");
+			System.out.println(e);
+	}
+
+}
